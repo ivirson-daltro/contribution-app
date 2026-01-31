5169cca699eafa1bcd521162f83b7a3486e26476
Ivirson Daltro <ivi.daltro@gmail.com>
2026-01-31 10:25:34 -0300

    Ajuste basehref

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
@@ -9,7 +9,7 @@ import { authTokenInterceptor } from './core/interceptors/auth-token.interceptor
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(
       withInterceptors([loadingInterceptor, authTokenInterceptor]),
       withInterceptorsFromDi(),
