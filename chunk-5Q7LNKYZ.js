38d9fa66d566572088b0a76c3ee731f26ed4addc
ivirson-daltro <ivi.daltro@gmail.com>
2026-02-20 20:25:09 +0000

    Deploying to gh-pages from @ ivirson-daltro/contribution-app@4b7ca1219edf1fa4d0a9e83dad4bd0ebda57f6af 🚀

--- chunk-5Q7LNKYZ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var t=[{path:"",loadComponent:()=>import("./chunk-VOFHUU4K.js").then(o=>o.ExpensesComponent)}];export{t as EXPENSES_ROUTES};
---
+++ chunk-PGKVTIKU.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var t=[{path:"",loadComponent:()=>import("./chunk-NRSVGXFI.js").then(o=>o.ExpensesComponent)}];export{t as EXPENSES_ROUTES};
